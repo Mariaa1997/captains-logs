8c32862c823a1c224f3f0cd2b2ccd29d4b9a6bca
Mariaa1997 <mariaaguilar_95@yahoo.com>
2023-12-15 18:50:04 -0500

    final deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // Load ENV Variables
 const express = require('express'); //import express
 const methodOverride = require('method-override'); 
 const mongoose = require('mongoose')
+const routes = require('./controllers/log')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -23,6 +24,8 @@ app.set('view engine', 'jsx');
 app.set('views', './views');
 app.engine('jsx', jsxViewEngine());
 
+//to connect to log.js
+app.use('/logs', routes)
 //MiddleWare
 app.use((req, res, next) => {
     console.log('Middleware: I run for all routes');
@@ -38,98 +41,6 @@ app.get('/', (req, res) => {
     res.send(`<h1>Captain's Logs</h1> <a href = ${'/logs'} > Go to Index Page</a> <br/>`)
    
 })
-//Index route
-app.get('/logs/', async (req, res) => {
-    // res.send(fruits);
-    try {
-        const foundLogs = await Logs.find({});
-        res.status(200).render('Index', {logs: foundLogs});
-    } catch (err) {
-        res.status(400).send(err);
-    }
-    
-});
-
-
-// New Route
-app.get('/new', (req, res) => {
-//     res.send("<h1>New Page</h1>")
-res.render('New')
-})
-
-// D - DELETE - PERMANENTLY removes a fruit from the db
-app.delete('/logs/:id', async (req, res)=> {
-    // res.send('deleting...');
-    try{
-        const deletedLogs = await Logs.findByIdAndDelete(req.params.id);
-        console.log(deletedLogs)
-        res.status(200).redirect('/logs');
-    } catch (err) {
-        res.status(400).send(err);
-    }
-
-})
-
-//U - UPDATE - makes the actual changes to the database
-app.put('/logs/:id', async (req, res) => {
-    if (req.body.shipIsBroken === 'on') {
-        req.body.shipIsBroken = true;
-    } else {
-        req.body.shipIsBroken = false;
-    }
-
-    try {
-        const updatedLogs = await Logs.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true },
-        )
-        console.log(updatedLogs);
-        res.status(200).redirect(`/logs/${req.params.id}`);
-    } catch (err) {
-    res.status(400).send(err)
-}})
-
-
-//Create Route
-app.post('/logs', async (req, res)=> {
-    if(req.body.shipIsBroken === 'on') {
-        req.body.shipIsBroken = true;
-    } else {
-        req.body.shipIsBroken = false;
-    }
-    try {
-        const createdLogs = await Logs.create(req.body);
-        res.status(200).redirect('/logs');
-    } catch (err) {
-        res.status(400).send(err);
-    }
-    // res.send(req.body)
-    // console.log(req.body)
-})
-
- // E -EDIT - allows a user to privide the inputs to change the fruit
- app.get('/logs/:id/edit', async (req, res)=> {
-    try{
-        const foundLog = await Logs.findById(req.params.id);
-        res.status(200).render("Edit", {log: foundLog});
-    } catch (err) {
-        res.status(400).send(err)
-    }
-    
- })
-
-// S - SHOW - show route displays details of an individual fruit
-app.get('/logs/:id', async (req, res) => {
-    // res.send(fruits[req.params.indexOfFruitsArray]);
-    try {
-        const foundLog = await Logs.findById(req.params.id)
-        res.render('Show', { log: foundLog});
-    }catch (err){
-        res.status(400).send(err);
-    }
-})
-
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
